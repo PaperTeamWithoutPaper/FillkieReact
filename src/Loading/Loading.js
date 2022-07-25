26d3ec93fcaf6140401908b8402af63800523a95
emulsion199 <wodnjs471@naver.com>
2022-07-25 21:35:14 +0900

    feat: add Project list loading

--- src/Loading/Loading.js
+++ src/Loading/Loading.js
@@ -1,6 +1,9 @@
 const Loading=()=>
 {
     return(
-        <img src=""></img>
+        <div>
+            <img style={{transform:"translate(calc(50vw - 100%),calc(50vh - 100% + 80px))"}} src={require("./loading.gif")}></img>
+        </div>
     )
 }
+export default Loading
