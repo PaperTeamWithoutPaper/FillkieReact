a5b4b24109418b0f9b415437b237e286a32a42f2
emulsion199 <wodnjs471@naver.com>
2022-09-13 20:33:24 +0900

    feat: Editor react pdf canvas size, position fit

--- src/Editor/Pdf/index.js
+++ src/Editor/Pdf/index.js
@@ -5,21 +5,29 @@ import { pdfjs } from 'react-pdf';
 import pdf from './test.pdf'
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 const MyDocument=()=> {
-  const [numPages, setNumPages] = useState(null);
+  const [numPages, setNumPages] = useState([]);
   const [pageNumber, setPageNumber] = useState(1);
 
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
+  function onDocumentLoadSuccess(params) {
+    var tempList=[]
+    console.log(params.getPage(1).width)
+    for(var i=1;i<=params.numPages;i++)
+    {
+        tempList.push(i)
+    }
+    setNumPages(tempList);
   }
 
   return (
-    <div style={{position:'fixed',left:'0px',top:'0px',zIndex:'1'}}>
-      <Document file={pdf} width="100"onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
+    <div style={{ position:'fixed',left:'0px',top:'0px',zIndex:'0',transform:'scale(1)'}}>
+        <Document pageLayout='oneColumn' file={pdf}  onLoadSuccess={onDocumentLoadSuccess}>
+        {
+            numPages.map((pageNumber)=>{return(
+                <Page wrap={false} size="A4" height={window.innerHeight} pageNumber={pageNumber} />
+            )})
+        }
+        </Document>
+
     </div>
   );
 }
